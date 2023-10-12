66f35eb138d7b1ec8834f04e305e3aa58b4fc22c
papihoudini <ochiengcliff.co@gmail.com>
2023-10-12 14:52:46 -0400

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Rank country origins of bands by the number of (non-unique) fans
 SELECT origin AS origin,
-  SUM(nb_fans) AS nb_fans
+  SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
